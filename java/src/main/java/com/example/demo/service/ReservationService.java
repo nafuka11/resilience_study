7757db40208687ab7adfb6c2b3099e0940ae007e
Kensuke Ishida <kensuke@dreamarts.co.jp>
2022-03-09 10:36:20 +0900

    implement retry

--- java/src/main/java/com/example/demo/service/ReservationService.java
+++ java/src/main/java/com/example/demo/service/ReservationService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
-import com.example.demo.config.Resilience4JConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.vavr.CheckedFunction1;
+import io.github.resilience4j.core.IntervalFunction;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
@@ -18,23 +21,47 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @Service
 public class ReservationService {
 
-    private final OkHttpClient client = new OkHttpClient.Builder()
-            .connectTimeout(Duration.ofMillis(1000))
+    private final OkHttpClient client = new OkHttpClient.Builder().connectTimeout(Duration.ofMillis(1000))
             .callTimeout(Duration.ofMillis(1000))
             .build();
 
     private static final String endpoint = "http://localhost:9081/api/go/slow";
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.of(
+            CircuitBreakerConfig.custom() //
+                    .failureRateThreshold(50) // 失敗として open にする閾値(%)
+                    .slowCallRateThreshold(50) // 呼び出し時間が slowCallDurationThreshold を超えて遅延とみなす閾値(%)
+                    .slowCallDurationThreshold(Duration.ofMillis(500)) // 呼び出しが遅延していると判断する時間
+                    .waitDurationInOpenState(Duration.ofSeconds(60)) // open から half-open に遷移するまでの待ち時間
+                    .permittedNumberOfCallsInHalfOpenState(3) // half-open のときに許可される呼び出し回数
+                    .minimumNumberOfCalls(10) // CircuitBreaker がエラー率を計算するようになるまでの呼び出し回数
+                    .slidingWindowType(
+                            CircuitBreakerConfig.SlidingWindowType.TIME_BASED) // 直近の slidingWindowSize 秒間の呼び出しを記録
+                    .slidingWindowSize(5) // 直近5秒間の記録で失敗を判定する
+                    .recordExceptions(IOException.class, TimeoutException.class) // 失敗として判定する例外
+                    .build());
+
+    private final RetryRegistry retryRegistry = RetryRegistry.of(RetryConfig.custom() //
+            .maxAttempts(3) // 最大リトライ回数
+            // .waitDuration(Duration.ofMillis(500)) // 固定の待ち時間
+            .intervalFunction(IntervalFunction.ofExponentialBackoff(IntervalFunction.DEFAULT_INITIAL_INTERVAL,
+                    IntervalFunction.DEFAULT_MULTIPLIER, 1000)) // リトライ回数ごとの待ち時間の決定関数をセット
+            .retryOnResult(result -> result == null) // 結果が null だったときにリトライする
+            //            .intervalFunction(numAttempts -> {
+            //                log.info("numAttempts={}", numAttempts);
+            //                return 100L;
+            //            }) //
+            .retryExceptions(IOException.class) // リトライする例外を定義
+            .build());
 
     @Autowired
-    private Resilience4JConfiguration resilience4JConfiguration;
+    private ObjectMapper objectMapper;
 
     public Reservation newReservation() {
         var reservation = new Reservation();
@@ -54,37 +81,29 @@ public class ReservationService {
     private Room getAvailableRoom(Date beginDate) {
         var circuitBreaker = getCircuitBreaker();
 
-        var callable = CircuitBreaker.decorateCallable(circuitBreaker, () -> {
-            Request request = new Request.Builder()
-                    .url(endpoint)
-                    .build();
+        var callableWithCircuitBreaker = CircuitBreaker.decorateCallable(circuitBreaker, () -> {
+            Request request = new Request.Builder().url(endpoint).build();
             try (Response response = client.newCall(request).execute()) {
                 Room[] rooms = objectMapper.readValue(response.body().string(), Room[].class);
                 if (rooms != null && rooms.length > 0) {
                     return rooms[0];
                 }
                 return null;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
         });
 
-        return Try.ofCallable(callable)
-                .recover(exception -> {
-                    var defaultRoom = new Room();
-                    defaultRoom.setId(0);
-                    defaultRoom.setName("Default");
-                    defaultRoom.setDescription("");
-                    return defaultRoom;
-                })
-                .get();
+        var retry = getRetry();
+
+        var callableWithRetry = Retry.decorateCallable(retry, callableWithCircuitBreaker);
+
+        return Try.ofCallable(callableWithRetry).recover(exception -> null).get();
     }
 
-    /**
-     * Goのサービスを呼び出す際のサーキットブレーカー
-     * @return
-     */
     private CircuitBreaker getCircuitBreaker() {
-        return resilience4JConfiguration.getCircuitBreakerRegistry().circuitBreaker("go/slow");
+        return circuitBreakerRegistry.circuitBreaker("go/slow");
+    }
+
+    private Retry getRetry() {
+        return retryRegistry.retry("go/slow");
     }
 }
